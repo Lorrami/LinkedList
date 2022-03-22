82af1272de6084c804b37664f5eeedf3f89cb897
Lorrami <wwwromchelo040608@gmail.com>
2022-03-22 15:42:08 +0100

    Adding LinkedList main functions

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "LinkedList.h"
 
 LinkedList::LinkedList()
@@ -5,6 +6,37 @@ LinkedList::LinkedList()
     head = nullptr;
 }
 
-void LinkedList::Add(int element) {}
+void LinkedList::Add(int element)
+{
+    Unit *newUnit = new Unit;
 
-void LinkedList::Print() {}
+    newUnit->data = element;
+    newUnit->next = nullptr;
+
+    if(head == nullptr)
+    {
+        head = newUnit;
+        std::cout << "Head added" << std::endl;
+    }
+    else
+    {
+        std::cout << "Added" << std::endl;
+        Unit *current = head;
+
+        while(current->next != nullptr)
+            current = current->next;
+
+        current->next = newUnit;
+    }
+}
+
+void LinkedList::Print()
+{
+    Unit *current = head;
+
+    while (current != nullptr)
+    {
+        std::cout << current->data << std::endl;
+        current = current->next;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,13 @@
+#include "LinkedList.h"
 
+int main()
+{
+    LinkedList myList;
+
+    myList.Add(34);
+    myList.Add(45);
+    myList.Add(67);
+
+    myList.Print();
+    return 777;
+}
