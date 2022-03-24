fc02757bec12430ae5247724ad2464cf5604668e
Lorrami <wwwromchelo040608@gmail.com>
2022-03-24 14:24:30 +0100

    Some fixes

--- main.cpp
+++ main.cpp
@@ -4,26 +4,18 @@
 int main()
 {
     LinkedList<std::string> myList;
-
     myList.Add("Red");
     myList.Add("Green");
     myList.Add("Blue");
-
     //myList.Remove("Red");
-
-    for (auto it = myList.begin(); it != myList.end(); ++it)
-    {
-        if (*it == "Green")
-        {
+    for (auto it = myList.begin(); it != myList.end(); ++it) {
+        if (*it == "Green") {
             myList.InsertAfter(it, "Imposter");
         }
-        if (*it == "Red")
-        {
+        if (*it == "Red") {
             myList.RemoveAfter(it);
         }
     }
-
     std::cout << myList;
-
     return 777;
 }
